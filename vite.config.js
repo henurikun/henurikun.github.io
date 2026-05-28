4e0b6932b697917d4ff305ae3dd01ecd776bb985
R e i <Henurikun14@example.com>
2026-05-28 21:46:00 +0800

    vite update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/nacorda-henry.github.io/",
+  base: "/henurikun.github.io/",
   plugins: [react()],
   server: {
     port: 5500,
