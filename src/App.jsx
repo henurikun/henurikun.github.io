81ccb52e8bf49657ba85e31923c23b3378e947d0
R e i <Henurikun14@example.com>
2026-06-25 00:30:56 +0800

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const skills = [
 
 const project = {
   name: "4RMTech Frontend",
-  description: "My first and currently only hosted project.",
+  description: "My first and current hosted project.",
   link: "https://4rmtech-frontend.vercel.app/",
 };
 
